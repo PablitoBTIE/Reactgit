2fbf6a8c78352232ca001a1acec185f9af0a6992
Your Name <you@example.com>
2023-10-30 09:31:57 +0100

    Add PolishKing component

---
+++ propsy/PolishKing.js
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+
+const PolishKing = ()=>{
+
+    return(
+        <View>
+            <Text>Polish King is 
+                comming</Text>
+        </View>
+    )
+}
+
+export default PolishKing;
